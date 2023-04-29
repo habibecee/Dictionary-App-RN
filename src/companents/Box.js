20f674363f4c5299c97644f38ebd97f20002fd66
habibecee <habibe.ce1996@gmail.com>
2023-04-29 15:03:28 +0300

    svgr-package-added, styled-system-added

--- src/companents/Box.js
+++ src/companents/Box.js
@@ -1,8 +1,7 @@
-import styled from 'styled-components';
-import {color} from 'styled-system';
+import {View} from 'react-native';
+import {color, compose, size, space, flexbox} from 'styled-system';
+import styled from 'styled-components/native';
 
-const Box = styled.View`
-  ${color}
-`;
+const Box = styled(View)(compose(color, size, space, flexbox));
 
 export default Box;
